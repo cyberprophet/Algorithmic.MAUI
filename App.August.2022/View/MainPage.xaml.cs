93d41cc2fabcb5e0c34db21d54f06c4bb91dea27
SANGWOO PARK <cyberprophet@hanmail.net>
2022-09-16 00:25:34 +0900

    simple implementation of INotifyPropertyChanged with ObservableObject

---
+++ App.August.2022/View/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+namespace ShareInvest.View
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ App.August.2022/ViewModel/BaseViewModel.cs
@@ -0,0 +1,48 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ShareInvest.ViewModel
+{
+    public partial class BaseViewModel : ObservableObject /* INotifyPropertyChanged */
+    {
+        /*
+        public bool IsBusy
+        {
+            set
+            {
+                if (isBusy == value)
+                    return;
+
+                isBusy = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(IsNotBusy));
+            }
+            get => isBusy;
+        }
+        public string Title
+        {
+            set
+            {
+                if (title == value)
+                    return;
+
+                title = value;
+                OnPropertyChanged();
+            }
+            get => title;
+        }
+        public void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        */
+        public bool IsNotBusy => isBusy is false;
+
+        [ObservableProperty,
+         NotifyPropertyChangedFor(nameof(IsNotBusy))]
+        bool isBusy;
+
+        [ObservableProperty]
+        string title;
+    }
+}
